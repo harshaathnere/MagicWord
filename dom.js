d16cd90844bf0007bd66cabd729c13b3117a5100
errorr404 <dkbishwash@gmail.com>
2019-03-16 17:24:12 +0530

    added logics for showMe function

--- dom.js
+++ dom.js
@@ -5,11 +5,14 @@ myarr.push(document.getElementById("wordInput").value);
 }
 var count = 0;
 function showMe(){
-    var newItem = document.createElement("li")
-     var array.forEach(item => {
-        newItem.appendChild(myarr)
+    myarr.forEach(function(item){
+        var text = document.createTextNode(item)
+        var newItem = document.createElement("li")
+        newItem.appendChild(text)
+        document.getElementById("root").appendChild(newItem)
     })
-    document.getElementById("wordInput").appendChild(newItem)
-    count++ //increased the value of count by 1
-}
 
+
+    // document.getElementById("wordInput").appendChild(newItem)
+    // count++ //increased the value of count by 1
+}
