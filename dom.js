19eb75bc3f5d959f958c5b7e5d1623cc2d4c9aca
errorr404 <dkbishwash@gmail.com>
2019-03-16 19:07:00 +0530

    added logic for removing child elements

--- dom.js
+++ dom.js
@@ -5,6 +5,10 @@ myarr.push(document.getElementById("wordInput").value);
 }
 var count = 0;
 function showMe(){
+   var myNode = document.getElementById("root");
+   while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+}
     myarr.forEach(function(item){
         var text = document.createTextNode(item)
         var newItem = document.createElement("li")
