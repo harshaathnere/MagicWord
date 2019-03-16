0d0535a7f5b75575c687371e604883109d210bd6
harshaathnere <harshaathnere@gmail.com>
2019-03-16 17:13:16 +0530

    added removed function

--- dom.js
+++ dom.js
@@ -1,8 +1,15 @@
 var myarr = [];
 function  Words(){
 myarr.push(document.getElementById("wordInput").value);
-console.log(myarr);
+
 }
+var count = 0;
 function showMe(){
-    console.log(myarr);
+    var newItem = document.createElement("li")
+     var array.forEach(item => {
+        newItem.appendChild(myarr)
+    })
+    document.getElementById("wordInput").appendChild(newItem)
+    count++ //increased the value of count by 1
 }
+
